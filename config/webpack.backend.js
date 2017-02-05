42be77338c6e4789703fa12c5c31d996de06b1e2
Carlos Ravelo <ravelo.carlos@gmail.com>
2017-02-05 17:56:34 -0500

    feat(server): configured webpack to compile the server as well

---
+++ config/webpack.backend.js
@@ -0,0 +1,28 @@
+var paths = require('./paths');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: paths.serverIndexJs,
+    target: 'node',
+    node: {
+        __dirname: false,
+        __filename: false,
+    },
+    output: {
+        path: paths.backendBuild,
+        filename: 'backend.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.json$/,
+                loader: 'json'
+            },
+        ],
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production'),
+        }),
+    ],
+};
